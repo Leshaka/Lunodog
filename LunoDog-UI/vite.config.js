44385ce71a89adc4a53558367b68439cedbb593c
Leshaka <leshkajm@ya.ru>
2025-10-15 14:16:51 +0200

    https for vite

--- LunoDog-UI/vite.config.js
+++ LunoDog-UI/vite.config.js
@@ -1,6 +1,8 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
+import fs from 'fs'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
@@ -8,9 +10,14 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 export default defineConfig({
   server: {
     host: '127.0.0.1',
-    port: 3355
+    port: 3355,
+    https: {
+      key: fs.readFileSync('../certs/private.key'),
+      cert: fs.readFileSync('../certs/lunodog.crt'),
+    },
   },
   plugins: [
+    basicSsl(),
     vue(),
     vueDevTools(),
   ],
